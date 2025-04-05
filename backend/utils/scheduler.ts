436027f506390bc8f852bd7515afc946cfec3efe
lillie <onlinechronically@protonmail.com>
2025-04-05 12:10:17 -0400

    update: Node definition+add: exports

--- backend/utils/scheduler.ts
+++ backend/utils/scheduler.ts
@@ -1,6 +1,20 @@
-interface Node {
-    id?: number,
-    title?: string,
-    classes?: Node[],
-    count?: number
+export interface Node {
+  id: number;
+  titleValue?: string;
+  numberValue?: number;
+  attributes?: Attribute[];
+  courseID?: number;
+  dropdownChildren?: boolean;
+  preRecs: Node[];
+}
+
+export enum Attribute {
+  AH = "A&H",
+  SH = "S&H",
+  WC = "World Culture",
+  WL = "World Language",
+  NM = "N&M",
+  IW = "Intensive Writing",
+  EC = "English Composition",
+  MM = "Mathematical Modeling",
 }
