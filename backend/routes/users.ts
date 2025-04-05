d080848b0424fe736f62fc667607f21c82b9a73c
lillie <onlinechronically@protonmail.com>
2025-04-05 14:18:43 -0400

    add: User Creation

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,6 +1,6 @@
 import Router from "express"
 import internalRequest from "../graphql/internal"
-import { hashPassword, signToken } from "../utils/auth"
+import { createSalt, hashPassword, signToken } from "../utils/auth"
 import { User } from "../graphql/db-types"
 
 const router = Router()
@@ -25,6 +25,31 @@ router.post("/verify", (req, res) => {
                 if (user.password == hashedPassword) return signToken(user)
                 else throw new Error()
             })
+            .then(token => res.status(200).cookie("token", token, { maxAge: 7*24*60*60*1000, httpOnly: true }).json({}))
+            .catch(_ => {
+                res.sendStatus(401)
+            })
+    }
+})
+
+router.post("/", (req, res) => {
+    if (req.body.username && req.body.password) {
+        const salt = createSalt(32)
+        hashPassword(req.body.password, salt)
+            .then(password => internalRequest(`
+                mutation AddUser($user: AddUserInput!) {
+                    addUser(user: $user) {
+                        id
+                        password
+                        salt
+                        username
+                    }
+                }`, { user: { username: req.body.username, salt, password } }))
+            .then(user => {
+                if (user && user.addUser) return user.addUser as User
+                else throw new Error()
+            })
+            .then(signToken)
             .then(token => res.status(200).cookie("token", token, { maxAge: 7*24*60*60*1000, httpOnly: true }).json({}))
             .catch(_ => {
                 res.sendStatus(401)
