fd58db61b187a34e314e6febbff8877e53daff61
lillie <onlinechronically@protonmail.com>
2025-04-04 22:06:50 -0400

    add: dotenv initialization

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+
+dotenv.config()
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
