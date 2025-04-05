068efa216169f2f0aa412dffb0861f44070c100e
lillie <onlinechronically@protonmail.com>
2025-04-05 15:28:33 -0400

    add: function for getting all course ids in a dept

--- backend/utils/scheduler.ts
+++ backend/utils/scheduler.ts
@@ -1,3 +1,6 @@
+import { DepartmentCourses } from "../graphql/db-types";
+import internalRequest from "../graphql/internal"
+
 export interface Node {
   id: number;
   titleValue?: string;
@@ -19,4 +22,24 @@ export enum Attribute {
   IW = "Intensive Writing",
   EC = "English Composition",
   MM = "Mathematical Modeling",
+}
+
+export function departmentCourses(name: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        internalRequest(`
+            query Departments($name: String!) {
+                department_name(name: $name) {
+                    courses {
+                        id
+                    }
+                }
+            }`, { name })
+                .then(data => {
+                    if (data && data.department_name) return data.department_name as DepartmentCourses
+                    else throw new Error()
+                })
+                .then((data: DepartmentCourses) => data.courses.map(course => course.id))
+                .then(resolve)
+                .catch(e => reject(e))
+    })
 }
