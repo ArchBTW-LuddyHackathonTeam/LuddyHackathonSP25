92fc2a8ca22a8b9e444a53b206a2500e7caef7d7
matthewivan <widjajamatthewivan@gmail.com>
2025-04-06 05:04:57 +0000

    Revert llm-webscraper

--- backend/routes/scheduler.ts
+++ backend/routes/scheduler.ts
@@ -1,10 +1,10 @@
 import Router from "express"
-import { queryPrompt } from "../utils/llm-webscraper"
+import { queryPrompt } from "../utils/openai"
 
 const router = Router()
 
 router.post("/prompt", (req, res) => {
-    if (req.body.prompt && req.body.input) queryPrompt(req.body.prompt).then(message => res.json({ input: req.body.input, message }))
+    if (req.body.prompt && req.body.input) queryPrompt("", req.body.prompt).then(message => res.json({ input: req.body.input, message }))
     else res.status(403).json({ message: "An unknown error occurred" })
 })
 
